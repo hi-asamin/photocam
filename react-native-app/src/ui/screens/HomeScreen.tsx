f95c8046bc312832c47717edf07044cf660f7d1b
yutarofukui <82759203+yutaro-fukui@users.noreply.github.com>
2021-07-02 01:58:04 +0900

    create HomeScreen

--- react-native-app/src/ui/screens/HomeScreen.tsx
+++ react-native-app/src/ui/screens/HomeScreen.tsx
@@ -1,28 +1,86 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, View, ScrollView, FlatList, Text } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
 
-import { TIMELINE, PROFILE, bottomTabs } from 'src/config/screens';
+import { ListImages } from 'src/ui/components/molecules/ListImages';
+import { TrendImage } from 'src/ui/components/atoms/TrendImage';
 
-import message from 'src/i18n/ja.json';
+import constant from 'src/i18n/ja.json';
 
 export const HomeScreen = () => {
-  const { navigate } = useNavigation();
-  return (
+  const renderItem = ({ item }: { item: TrendData }) => (
     <View>
-      <Text>{message.home.title}</Text>
-      <Button
-        title="プロフィール画面"
-        onPress={() => {
-          navigate(bottomTabs.profile, { screen: PROFILE });
-        }}
-      />
-      <Button
-        title="タイムライン画面"
-        onPress={() => {
-          navigate(TIMELINE);
-        }}
-      />
+      <TrendImage imageUrl={item.imageUrl} trendText={item.trendText} />
     </View>
   );
+  return (
+    <ScrollView>
+      <View style={styles.trendContainer}>
+        <Text style={styles.trendTitle}>{constant.label.trend}</Text>
+        <View style={styles.trendImageList}>
+          <FlatList
+            data={trendList}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.key}
+            numColumns={2}
+          />
+        </View>
+      </View>
+      <View style={styles.imageListContainer}>
+        <ListImages />
+      </View>
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  trendContainer: {
+    flex: 1,
+    paddingHorizontal: 7,
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
+  imageListContainer: {
+    flex: 1,
+  },
+  trendImageList: {
+    alignItems: 'center',
+  },
+  trendTitle: {
+    fontSize: 17,
+    fontWeight: '700',
+    color: '#808080',
+  },
+});
+
+// トレンド表示用画像データ
+export interface TrendData {
+  key: string;
+  imageUrl: string;
+  trendText: string;
+}
+
+const trendList: TrendData[] = [
+  {
+    key: '1',
+    imageUrl:
+      'https://www.campnofuji.jp/column/cms/wp-content/uploads/2021/02/img_solo-camp_003.jpg',
+    trendText: 'ソロキャンプ',
+  },
+  {
+    key: '2',
+    imageUrl: 'https://www.resort-glamping.com/wp-content/uploads/2019/12/skyhillglamping12.jpg',
+    trendText: 'グランピング',
+  },
+  {
+    key: '3',
+    imageUrl: 'https://p0bcd8hr.user.webaccel.jp/wp-content/uploads/2015/12/IMGP7275.jpg',
+    trendText: 'ロースタイル',
+  },
+  {
+    key: '4',
+    imageUrl:
+      'https://p1-16efdeae.imageflux.jp/default/image/image/964774288381818986/article_pc_GettyImages-821832652.jpg',
+    trendText: '女子キャンプ',
+  },
+];
