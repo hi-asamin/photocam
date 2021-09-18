9e98f69cb0e4926306cd4c61c3b2c1cef9a75576
marc-town <asamin.itconsultant@gmail.com>
2021-09-18 17:34:55 +0900

    install firebase

--- react-native-app/App.tsx
+++ react-native-app/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Navigator } from 'src/navigator';
 
+import firebase from 'firebase';
+import { firebaseConfig } from 'src/config/firebase';
+
+require('firebase/firestore');
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 function App() {
   return <Navigator />;
 }
